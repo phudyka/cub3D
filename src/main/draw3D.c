040f20693d1c102cc92cfd51190b0a22b4218dbc
David TASSEL <davidtassel04@gmail.com>
2023-10-05 11:28:21 -0400

    floor

--- src/main/draw3D.c
+++ src/main/draw3D.c
@@ -3,28 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   draw3D.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dtassel <dtassel@42.nice.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:52:20 by phudyka           #+#    #+#             */
-/*   Updated: 2023/10/05 15:32:43 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/10/05 11:24:57 by dtassel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/main.h"
 
-static void	draw_column(int x, int y_start, int y_end, int color, t_cub *game)
+/*void draw_ceiling(int x, int draw_end, t_cub *game)
 {
-	int	y;
-	int	dst_index;
+    double posZ = 0.5 * HEIGHT; 
+    int y = 0;
 
-	y = y_start;
-	while (y < y_end)
-	{
-		dst_index = (y * game->img3d.size_line) + (x * game->img3d.bpp / 8);
-		*(unsigned int *)(game->img3d.pixels + dst_index) = color;
-		y++;
-	}
-} 
+    while (y < draw_end)
+    {
+        double p = y - HEIGHT / 2;
+        double rowDistance = posZ / p;
+        double floorStepX = rowDistance * (game->ray.dir_x + game->ray.plane_x - (game->ray.dir_x - game->ray.plane_x)) / WIDTH;
+        double floorStepY = rowDistance * (game->ray.dir_y + game->ray.plane_y - (game->ray.dir_y - game->ray.plane_y)) / WIDTH;
+        double floorX = game->ray.player_x + rowDistance * game->ray.dir_x;
+        double floorY = game->ray.player_y + rowDistance * game->ray.dir_y;
+        floorX += floorStepX * (x - (HEIGHT / 2));
+        floorY += floorStepY * (x - (HEIGHT / 2));
+        int textureX = (int)(floorX * 256) % 256;
+        int textureY = (int)(floorY * 256) % 256;
+        int color = ft_colorpix(textureX, textureY, game->texture.ceiling, game);
+        int index_floor = (y * game->img3d.size_line) + (x * game->img3d.bpp / 8);
+        *(unsigned int *)(game->img3d.pixels + index_floor) = color;
+
+        y++;
+    }
+}*/
+
+void draw_floor(int x, int draw_end, t_cub *game)
+{
+    double posZ = 0.5 * HEIGHT; 
+    int y = draw_end + 1;
+
+    while (y < HEIGHT)
+    {
+        double p = y - HEIGHT / 2;
+        double rowDistance = posZ / p;
+        double floorStepX = rowDistance * (game->ray.dir_x + game->ray.plane_x - (game->ray.dir_x - game->ray.plane_x)) / WIDTH;
+        double floorStepY = rowDistance * (game->ray.dir_y + game->ray.plane_y - (game->ray.dir_y - game->ray.plane_y)) / WIDTH;
+        double floorX = game->ray.player_x + rowDistance * game->ray.dir_x;
+        double floorY = game->ray.player_y + rowDistance * game->ray.dir_y;
+        floorX += floorStepX * (x - (WIDTH / 2));
+        floorY += floorStepY * (x - (WIDTH / 2));
+        int textureX = (int)(floorX * 256) % 256;
+        int textureY = (int)(floorY * 256) % 256;
+        int color = ft_colorpix(textureX, textureY, game->texture.floor, game);
+        int index_floor = (y * game->img3d.size_line) + (x * game->img3d.bpp / 8);
+        *(unsigned int *)(game->img3d.pixels + index_floor) = color;
+
+        y++;
+    }
+}
 
 static void ft_texture_index(t_cub *game)
 {
@@ -103,8 +139,10 @@ void render_3D(int x, t_cub *game)
     else
         game->ray.wall_x = game->ray.player_x + game->ray.distance * game->ray.ray_x;
     game->ray.wall_x -= floor(game->ray.wall_x);
-    draw_column(x, 0, game->ray.draw_start, BLUE, game);
     choose_texture(x, game->ray.draw_start, game->ray.draw_end, game);
+    //draw_ceiling(x, game->ray.draw_start, game);
+    draw_floor(x, game->ray.draw_end, game);
+    //draw_column(x, 0, game->ray.draw_start, GREY, game);
     //draw_texture(x, 0, game->ray.drawStart, CEILING, game);
-    draw_column(x, game->ray.draw_end, HEIGHT, BROWN, game);
+    //draw_column(x, game->ray.draw_end, HEIGHT, BROWN, game);
 }
