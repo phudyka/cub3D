81f2f27cc940a02a8f3c81c1e97339a96e95e724
phudyka <paul.hudyka@gmail.com>
2023-10-13 17:36:34 +0200

    map parsing broken

--- src/main/texture_utils.c
+++ src/main/texture_utils.c
@@ -6,7 +6,7 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:24:31 by phudyka           #+#    #+#             */
-/*   Updated: 2023/10/13 16:39:33 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/10/13 17:25:35 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,19 @@ static void	ft_init_walls(t_cub *game)
 	game->texture.north = mlx_xpm_file_to_image(game->mlx,
 			game->texture.path_north, &w, &h);
 	if (game->texture.north == (void *)0)
-		ft_error_free("Error!\n[sprite/game/north_wall.xpm]]\n", game);
+		ft_error_free("Error!\n[sprite/game/north_wall.xpm]\n", game);
 	game->texture.south = mlx_xpm_file_to_image(game->mlx,
 			game->texture.path_south, &w, &h);
 	if (game->texture.south == (void *)0)
-		ft_error_free("Error!\n[sprite/game/south_wall.xpm]]\n", game);
+		ft_error_free("Error!\n[sprite/game/south_wall.xpm]\n", game);
 	game->texture.east = mlx_xpm_file_to_image(game->mlx,
 			game->texture.path_west, &w, &h);
 	if (game->texture.east == (void *)0)
-		ft_error_free("Error!\n[sprite/game/east_wall.xpm]]\n", game);
+		ft_error_free("Error!\n[sprite/game/east_wall.xpm]\n", game);
 	game->texture.west = mlx_xpm_file_to_image(game->mlx,
 			game->texture.path_east, &w, &h);
 	if (game->texture.west == (void *)0)
-		ft_error_free("Error!\n[sprite/game/west_wall.xpm]]\n", game);
+		ft_error_free("Error!\n[sprite/game/west_wall.xpm]\n", game);
 }
 
 void	ft_init_mlx(t_cub *game)
